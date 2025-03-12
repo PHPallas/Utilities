d9f5ce03762adcd6fbe3f9ff56b19f6277f73af2
Sina Kuhestani <sinakuhestani@gmail.com>
2025-03-12 20:36:11 +0330

    ...

--- src/ArrayUtility.php
+++ src/ArrayUtility.php
@@ -6,129 +6,274 @@ namespace PHPallas\Utilities;
 
 class ArrayUtility
 {
-    public static function create (mixed ...$elements): array
+    /**
+     * Create an array
+     * @param mixed[] $elements
+     * @return array
+     */
+    public static function create(mixed ...$elements): array
     {
         return [...$elements];
     }
-    public static function createRange (float|int $min, float|int $max, float|int $step = 1): array
+
+    /**
+     * Create an array containing a range of elements.
+     * @param float|int $min
+     * @param float|int $max
+     * @param float|int $step
+     * @return array
+     */
+    public static function createRange(float|int $min, float|int $max, float|int $step = 1): array
     {
-        return range ($min, $max, $step);
+        return range($min, $max, $step);
     }
+
+    /**
+     * Create an array with same value and given size
+     * @param int $count
+     * @param mixed $value
+     * @return array
+     */
     public static function createByValue(int $count, mixed $value): array
     {
         $output = [];
-        for($i = 0; $i< $count; $i++)
-        {
+        for ($i = 0; $i < $count; $i++) {
             $output[] = $value;
         }
         return $output;
     }
+
+    /**
+     * Create an array with same value and given keys
+     * @param array $keys
+     * @param mixed $value
+     * @return array
+     */
     public static function createByKeys(array $keys, mixed $value): array
     {
         $output = [];
-        foreach($keys as $key)
-        {
+        foreach ($keys as $key) {
             $output[$key] = $value;
         }
         return $output;
     }
+
+    /**
+     * Checks if the given key or index exists in the array
+     * @param array $array
+     * @param int|string $key
+     * @return bool
+     */
     public static function isset(array $array, int|string $key): bool
     {
         return array_key_exists($key, $array);
     }
+
+    /**
+     * Gets the first key of an array
+     * @param array $array
+     * @return int|string|null
+     */
     public static function firstKey(array $array): mixed
     {
         return array_key_first($array);
     }
+
+    /**
+     * Gets the last key of an array
+     * @param array $array
+     * @return int|string|null
+     */
     public static function lastKey(array $array): mixed
     {
         return array_key_last($array);
     }
+
+    /**
+     * Gets the first element of an array
+     * @param array $array
+     * @return mixed
+     */
     public static function first(array $array): mixed
     {
-        $key =static::firstKey($array);
+        $key = static::firstKey($array);
         return $array[$key] ?? null;
     }
+
+    /**
+     * Gets the last element of an array
+     * @param array $array
+     * @return mixed
+     */
     public static function last(array $array): mixed
     {
         $key = static::lastKey($array);
         return $array[$key] ?? null;
     }
+
+    /**
+     * Drop n first element(s) of an array
+     * @param array $array
+     * @param int $count
+     * @return array
+     */
     public static function dropFirst(array $array, int $count = 1): mixed
     {
-        $n = min (count($array), $count);
+        $n = min(count($array), $count);
         for ($i = 0; $i < $n; $i++) {
             array_shift($array);
         }
         return $array;
     }
+
+    /**
+     * Drop n last element(s) of an array
+     * @param array $array
+     * @param int $count
+     * @return array
+     */
     public static function dropLast(array $array, int $count = 1): mixed
     {
-        $n = min (count($array), $count);
+        $n = min(count($array), $count);
         for ($i = 0; $i < $n; $i++) {
             array_pop($array);
         }
         return $array;
     }
+
+    /**
+     * Append elements into the end of an array
+     * @param array $array
+     * @param mixed[] $values
+     * @return array
+     */
     public static function append(array $array, mixed ...$values): array
     {
         array_push($array, ...$values);
         return $array;
     }
+
+    /**
+     * Append elements into the start of an array
+     * @param array $array
+     * @param mixed[] $values
+     * @return array
+     */
     public static function prepend(array $array, mixed ...$values): array
     {
         array_unshift($array, ...$values);
         return $array;
     }
+
+    /**
+     * Get total number of elements inside an array
+     * @param array $array
+     * @return int
+     */
     public static function size(array $array): int
     {
         $size = 0;
         foreach ($array as $value) {
             if (true === is_array($value)) {
                 $size += static::size($value);
-            }
-            else {
+            } else {
                 $size++;
             }
         }
         return $size;
     }
+
+    /**
+     * Changes the case of all keys in an array to UPPER_CASE
+     * @param mixed $array
+     * @return array
+     */
     public static function uppercaseKeys($array): array
     {
         return array_change_key_case($array, CASE_UPPER);
     }
+
+    /**
+     * Changes the case of all keys in an array to lower_case
+     * @param mixed $array
+     * @return array
+     */
     public static function lowercaseKeys($array): array
     {
         return array_change_key_case($array, CASE_LOWER);
     }
+
+    /**
+     * Transform all elements of an array subject to a function
+     * @param array $array
+     * @param callable $function
+     * @return array
+     */
     public static function transform(array $array, callable $function): array
     {
         foreach ($array as &$element) {
-            $element = $function ($element);
+            $element = $function($element);
         }
         return $array;
     }
+
+    /**
+     * Transform all elements of an array to lower_case
+     * @param array $array
+     * @return array
+     */
     public static function toLower(array $array): array
     {
-        return static::transform($array, function ($item){return strtolower($item);});
+        return static::transform($array, function ($item) {
+            return strtolower($item); });
     }
+
+    /**
+     * Transform all elements of an array to UPPER_CASE
+     * @param array $array
+     * @return array
+     */
     public static function toUpper(array $array): array
     {
-        return static::transform($array, function ($item){return strtoupper($item);});
+        return static::transform($array, function ($item) {
+            return strtoupper($item); });
     }
-    public static function chuck(array $array, int $size, bool $isAssoc): array
+
+    /**
+     * Split an array into chunks
+     * @param array $array
+     * @param int $size
+     * @param bool $isAssoc
+     * @return array
+     */
+    public static function chunk(array $array, int $size, bool $isAssoc = false): array
     {
         return array_chunk($array, $size, $isAssoc);
     }
+
+    /**
+     * Get a subset of associative array
+     * @param array $array
+     * @param array $keys
+     * @return array
+     */
     public static function getKeys(array $array, array $keys): array
     {
         $output = [];
         foreach ($array as $key => $item) {
-            if (false === in_array($key, $keys)) continue;
+            if (false === in_array($key, $keys))
+                continue;
             $output[$key] = $item;
         }
         return $output;
     }
+
+    /**
+     * Get a subset of two-dimensions array by keys
+     * @param array $array
+     * @param array $columns
+     * @return array
+     */
     public static function getColumns(array $array, array $columns): array
     {
         foreach ($array as &$item) {
@@ -136,40 +281,79 @@ class ArrayUtility
         }
         return $array;
     }
+
+    /**
+     * Creates an `array` by using the values from the `keys` array as keys and 
+     * the values from the `values` array as the corresponding values.
+     * @param array $keys
+     * @param array $values
+     * @return array
+     */
     public static function pairedCombine(array $keys, array $values): array
     {
         return array_combine($keys, $values);
     }
+
+    /**
+     * Get count of distinct values inside an array
+     * @param array $array
+     * @return int[]
+     */
     public static function counts(array $array): array
     {
         $output = [];
         foreach ($array as $value) {
-            if (!isset($output[$value])) $output[$value] = 0;
+            if (!isset($output[$value]))
+                $output[$value] = 0;
             $output[$value]++;
         }
         return $output;
     }
-    public static function has (array $array, mixed $value): bool
+
+    /**
+     * Check if an array includes a given value
+     * @param array $array
+     * @param mixed $value
+     * @return bool
+     */
+    public static function has(array $array, mixed $value): bool
     {
         return in_array($value, $array, true);
     }
+
+    /**
+     * Compare two arrays and check if are same
+     * @param array $array1
+     * @param array $array2
+     * @param bool $strict
+     * @return bool
+     */
     public static function areSame(array $array1, array $array2, bool $strict = false): bool
     {
         if (false === $strict) {
             return 0 === count(array_diff($array1, $array2));
-        }
-        else {
+        } else {
             return 0 === count(array_diff_assoc($array1, $array2));
         }
     }
+
+    /**
+     * Filters elements of an array using a callback function
+     * @param array $array
+     * @param callable $function
+     * @return array
+     */
     public static function filter(array $array, callable $function): array
     {
-        return array_filter($array, $function, ARRAY_FILTER_USE_BOTH);
+        return array_values(array_filter($array, $function, ARRAY_FILTER_USE_BOTH));
     }
-    public static function search (array $array, mixed $value): array
-    {
-        return static::filter($array, function ($elem, $value){return $elem === $value;});
-    }
+
+    /**
+     * Set array items, supporting dot notation
+     * @param array $array
+     * @param string $path
+     * @param mixed $value
+     */
     public static function set(array &$array, string $path, mixed $value)
     {
         $location = &$array;
@@ -178,6 +362,13 @@ class ArrayUtility
         }
         return $location = $value;
     }
+
+    /**
+     * Get array items, supporting dot notation
+     * @param array $array
+     * @param string $path
+     * @param mixed $default
+     */
     public static function get(array &$array, string $path, mixed $default = null)
     {
         $location = &$array;
@@ -186,7 +377,9 @@ class ArrayUtility
         }
         return $location;
     }
-    public static function satisfies (array $array, callable $function): bool
+
+    
+    public static function satisfies(array $array, callable $function): bool
     {
         foreach ($array as $index => $value) {
             if (false === $function($index, $value)) {
@@ -215,29 +408,22 @@ class ArrayUtility
     public static function sort(array $array, $sortByKeys = false, $descending = false, $isAssoc = false): array
     {
         if (true === $sortByKeys) {
-            if (true === $descending)
-            {
+            if (true === $descending) {
                 krsort($array);
-            }
-            else {
+            } else {
                 ksort($array);
             }
-        }
-        else {
-            if (true === $descending)
-            {
+        } else {
+            if (true === $descending) {
                 if (true === $isAssoc) {
                     arsort($array);
-                }
-                else {
+                } else {
                     rsort($array);
                 }
-            }
-            else {
+            } else {
                 if (true === $isAssoc) {
                     asort($array);
-                }
-                else {
+                } else {
                     sort($array);
                 }
             }
---
+++ tests/ArrayUtilityTest.php
@@ -0,0 +1,390 @@
+<?php declare(strict_types=1);
+
+use PHPallas\Utilities\ArrayUtility;
+use PHPUnit\Framework\TestCase;
+
+final class ArrayUtilityTest extends TestCase
+{
+    public function testCreateArray(): void
+    {
+        $expectedArray = [1, 2, 3];
+        $createdArray = ArrayUtility::create(1, 2, 3);
+        $this->assertSame($expectedArray, $createdArray);
+    }
+    public function testCreateRangeArray(): void
+    {
+        $expectedArray = [5, 6, 7, 8, 9];
+        $createdArray = ArrayUtility::createRange(5, 9, 1);
+        $this->assertSame($expectedArray, $createdArray);
+        $expectedArray = [3, 6, 9];
+        $createdArray = ArrayUtility::createRange(3, 9, 3);
+        $this->assertSame($expectedArray, $createdArray);
+    }
+    public function testCreateByValue(): void
+    {
+        $expectedArray = [1, 1, 1, 1, 1];
+        $createdArray = ArrayUtility::createByValue(5, 1);
+        $this->assertSame($expectedArray, $createdArray);
+    }
+    public function testCreateByKeys(): void
+    {
+        $expectedArray = [
+            "apple" => 100,
+            "banana" => 100,
+            "orange" => 100,
+        ];
+        $createdArray = ArrayUtility::createByKeys(["apple", "banana", "orange"], 100);
+        $this->assertSame($expectedArray, $createdArray);
+    }
+
+    public function testIsset(): void
+    {
+        $array = [
+            "apple" => 100,
+            "banana" => 100,
+        ];
+        $this->assertSame(true, ArrayUtility::isset($array, "apple"));
+        $this->assertSame(true, ArrayUtility::isset($array, "banana"));
+        $this->assertSame(false, ArrayUtility::isset($array, "orange"));
+        $this->assertSame(false, ArrayUtility::isset($array, 0));
+        $array = ["apple", "banana",];
+        $this->assertSame(false, ArrayUtility::isset($array, "apple"));
+        $this->assertSame(false, ArrayUtility::isset($array, "banana"));
+        $this->assertSame(false, ArrayUtility::isset($array, "orange"));
+        $this->assertSame(true, ArrayUtility::isset($array, 0));
+        $this->assertSame(true, ArrayUtility::isset($array, 1));
+        $this->assertSame(false, ArrayUtility::isset($array, 2));
+    }
+    public function testFirstKey(): void
+    {
+        $array = [
+            "apple" => 100,
+            "banana" => 100,
+        ];
+        $this->assertSame("apple", ArrayUtility::firstKey($array));
+        $array = ["apple", "banana",];
+        $this->assertSame(0, ArrayUtility::firstKey($array));
+    }
+    public function testLastKey(): void
+    {
+        $array = [
+            "apple" => 100,
+            "banana" => 100,
+        ];
+        $this->assertSame("banana", ArrayUtility::lastKey($array));
+        $array = ["apple", "banana",];
+        $this->assertSame(1, ArrayUtility::lastKey($array));
+    }
+    public function testFirst(): void
+    {
+        $array = [
+            "apple" => 100,
+            "banana" => 100,
+        ];
+        $this->assertSame(100, ArrayUtility::first($array));
+        $array = ["apple", "banana",];
+        $this->assertSame("apple", ArrayUtility::first($array));
+    }
+    public function testLast(): void
+    {
+        $array = [
+            "apple" => 100,
+            "banana" => 200,
+        ];
+        $this->assertSame(200, ArrayUtility::last($array));
+        $array = ["apple", "banana",];
+        $this->assertSame("banana", ArrayUtility::last($array));
+    }
+    public function testDropFirst(): void
+    {
+        $array = [
+            "apple" => 100,
+            "banana" => 200,
+            "orange" => 300,
+        ];
+        $this->assertSame(
+            [
+                "banana" => 200,
+                "orange" => 300,
+            ],
+            ArrayUtility::dropFirst($array)
+        );
+        $this->assertSame(
+            [
+                "orange" => 300,
+            ],
+            ArrayUtility::dropFirst($array, 2)
+        );
+        $this->assertSame([], ArrayUtility::dropFirst($array, 3));
+        $this->assertSame([], ArrayUtility::dropFirst($array, 100));
+    }
+    public function testDropLast(): void
+    {
+        $array = [
+            "apple" => 100,
+            "banana" => 200,
+            "orange" => 300,
+        ];
+        $this->assertSame(
+            [
+                "apple" => 100,
+                "banana" => 200,
+            ],
+            ArrayUtility::dropLast($array)
+        );
+        $this->assertSame(
+            [
+                "apple" => 100,
+            ],
+            ArrayUtility::dropLast($array, 2)
+        );
+        $this->assertSame([], ArrayUtility::dropLast($array, 3));
+        $this->assertSame([], ArrayUtility::dropLast($array, 100));
+    }
+    public function testAppend(): void
+    {
+        $array = ["apple"];
+        $this->assertSame(["apple","orange",], ArrayUtility::append($array, "orange"));
+        $this->assertNotSame(["orange","apple",], ArrayUtility::append($array, "orange"));
+
+    }
+    public function testPrepend(): void
+    {
+        $array = ["apple"];
+        $this->assertSame(["orange","banana","apple",], ArrayUtility::prepend($array, "orange", "banana"));
+        $this->assertNotSame(["apple","orange","banana",], ArrayUtility::prepend($array, "orange", "banana"));
+
+    }
+    public function testSize(): void
+    {
+        $array = [
+            "apple",
+            "other" => [
+                "banana",
+                "orange"
+            ]
+        ];
+        $this->assertSame(3, ArrayUtility::size($array));
+        $array = [
+            "apple",
+            "banana",
+            "orange",
+            "other" => [
+                "banana",
+                "orange"
+            ]
+        ];
+        $this->assertSame(5, ArrayUtility::size($array));
+        $array = [
+            "apple",
+            "banana",
+            "orange",
+            "other" => [
+                "banana",
+                "orange",
+                "other" => [
+                    "appale",
+                    "appale",
+                ]
+            ]
+        ];
+        $this->assertSame(7, ArrayUtility::size($array));
+    }
+    public function testUppercaseKeys(): void
+    {
+        $array = ["apple" => 100, "orange" => 200];
+        $this->assertSame(["APPLE" => 100, "ORANGE" => 200], ArrayUtility::uppercaseKeys($array));
+
+    }
+    public function testLowercaseKeys(): void
+    {
+        $array = ["APPLE" => 100, "ORANGE" => 200];
+        $this->assertSame(["apple" => 100, "orange" => 200], ArrayUtility::lowercaseKeys($array));
+
+    }
+    public function testTransform(): void
+    {
+        $array = [1,4,9,16,25];
+        $this->assertSame([1,2,3,4,5], ArrayUtility::transform($array,function($item){return (int) sqrt($item);}));
+
+    }
+    public function testTolower(): void
+    {
+        $array = ["appLE", "OranGe"];
+        $this->assertSame(["apple", "orange"], ArrayUtility::tolower($array));
+
+    }
+    public function testToupper(): void
+    {
+        $array = ["appLE", "OranGe"];
+        $this->assertSame(["APPLE", "ORANGE"], ArrayUtility::toupper($array));
+
+    }
+    public function testChunk(): void
+    {
+        $array = [1,2,3,4,5,6,7,8,9];
+        $this->assertSame([[1,2,3],[4,5,6],[7,8,9]], ArrayUtility::chunk($array,3));
+        $this->assertSame([[1,2,3,4,5,6],[7,8,9]], ArrayUtility::chunk($array,6));
+        $array = [
+            "name" => "john",
+            "surname" => "doe",
+            "email" => "johndoe@example.com",
+            "country" => "Germany",
+            "city" => "Berlin",
+        ];
+        $this->assertSame([
+            [
+                "name" => "john",
+                "surname" => "doe",
+                "email" => "johndoe@example.com",
+            ],
+            [
+                "country" => "Germany",
+                "city" => "Berlin",
+            ]
+        ], ArrayUtility::chunk($array,3, true));
+    }
+    public function testGetKeys(): void
+    {
+        $array = [
+            "name" => "john",
+            "surname" => "doe",
+            "email" => "johndoe@example.com",
+            "country" => "Germany",
+            "city" => "Berlin",
+        ];
+        $this->assertSame([
+            "name" => "john",
+            "surname" => "doe",
+            "email" => "johndoe@example.com",
+        ], ArrayUtility::getKeys($array, ["name","surname","email",]));
+    }
+    public function testGetColumns(): void
+    {
+        $array = [
+            [
+                "name" => "john",
+                "surname" => "doe",
+                "email" => "johndoe@example.com",
+                "country" => "Germany",
+                "city" => "Berlin",
+            ],
+            [
+                "name" => "patt",
+                "surname" => "mike",
+                "email" => "patt@example.com",
+                "country" => "England",
+                "city" => "London",
+            ]
+            ];
+        $this->assertSame([
+            [
+                "name" => "john",
+                "surname" => "doe",
+                "email" => "johndoe@example.com",
+            ],
+            [
+                "name" => "patt",
+                "surname" => "mike",
+                "email" => "patt@example.com",
+            ]
+        ], ArrayUtility::getColumns($array, ["name","surname","email",]));
+    }
+    public function testPairedCombine(): void
+    {
+        $values = ["john","doe","johndoe@example.com"];
+        $keys = ["name","surname","email"];
+        $this->assertSame([
+            "name" => "john",
+            "surname" => "doe",
+            "email" => "johndoe@example.com",
+        ], ArrayUtility::pairedCombine($keys, $values));
+    }
+    public function testCount(): void
+    {
+        $array = ["john","mary","paul","john"];
+        $this->assertSame([
+            "john" => 2,
+            "mary" => 1,
+            "paul" => 1,
+        ], ArrayUtility::counts($array));
+    }
+    public function testHas(): void
+    {
+        $array = ["john","mary","paul","john"];
+        $this->assertSame(true, ArrayUtility::has($array,"john"));
+        $this->assertSame(true, ArrayUtility::has($array,"paul"));
+        $this->assertSame(false, ArrayUtility::has($array,"robert"));
+    }
+
+    public function testAreSame(): void
+    {
+        $array = ["john","mary","paul"];
+        $this->assertSame(true, ArrayUtility::areSame($array,["mary","john","paul"]));
+        $this->assertSame(false, ArrayUtility::areSame($array,["mary","john","paul"], true));
+        $this->assertSame(true, ArrayUtility::areSame($array,["john","mary","paul"]));
+        $this->assertSame(true, ArrayUtility::areSame($array,["john","mary","paul"], true));
+        $array = [
+            "name" => "john",
+            "age" => 18
+        ];
+        $this->assertSame(true, ArrayUtility::areSame($array,[
+            "name" => "john",
+            "age" => 18
+        ]));
+        $this->assertSame(true, ArrayUtility::areSame($array,[
+            "name" => "john",
+            "age" => 18
+        ],true));
+        $this->assertSame(true, ArrayUtility::areSame($array,[
+            "age" => 18,
+            "name" => "john",
+        ]));
+        $this->assertSame(true, ArrayUtility::areSame($array,[
+            "age" => 18,
+            "name" => "john",
+        ],true));
+    }
+    public function testFilter(): void
+    {
+        $array = [
+            [
+                "age" => 18,
+                "name" => "john",
+            ],
+            [
+                "age" => 22,
+                "name" => "paul",
+            ],
+        ];
+        $filter = function ($item) {
+            return $item["age"] > 20;
+        };
+        $this->assertSame([["age" => 22, "name" => "paul"]], ArrayUtility::filter($array,$filter));
+    }
+    public function testSet(): void
+    {
+        $array = [
+            "name" => [
+                "nickname" => "john",
+                "surname" => "doe"
+            ],
+            "address" => [
+                "city" => "London",
+                "country" => "England"
+            ]
+        ];
+        $array2 = $array;
+        ArrayUtility::set($array2, "name.nickname", "paul");
+        $this->assertSame([
+            "name" => [
+                "nickname" => "paul",
+                "surname" => "doe"
+            ],
+            "address" => [
+                "city" => "London",
+                "country" => "England"
+            ]
+        ], $array2);
+    }
+}
