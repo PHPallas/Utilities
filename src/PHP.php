209f382c46ec3c9507daa7de7bc76540ba837591
Sina Kuhestani <sinakuhestani@gmail.com>
2025-03-21 17:49:55 +0330

    PHP Utility

--- src/PHP.php
+++ src/PHP.php
@@ -6,14 +6,49 @@ namespace PHPallas\Utilities;
 
 class PHP
 {
+    /**
+     * Get PHP version
+     * @return bool|string
+     */
     public static function version(): string
     {
         return phpversion();
     }
+    /**
+     * Get PHP version as a number
+     * @return int
+     */
     public static function versionID(): int
     {
         $version = explode(".", phpversion());
         $value = 10000 * (int) $version[0] + 100 * (int) $version[1] + (int) $version[2];
         return $value;
+    }
+    /**
+     * Get PHP major version
+     * @return int|string
+     */
+    public static function versionMajor(): int
+    {
+        $version = explode(".", phpversion());
+        return $version[0] ?? 0;
+    }
+    /**
+     * Get PHP minor Version
+     * @return int|string
+     */
+    public static function versionMinor(): int
+    {
+        $version = explode(".", phpversion());
+        return $version[1] ?? 0;
+    }
+    /**
+     * Get PHP release version
+     * @return int|string
+     */
+    public static function versionRelease(): int
+    {
+        $version = explode(".", phpversion());
+        return $version[2] ?? 0;
     }
 }
