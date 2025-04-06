529beea2a75771ffacf34672d3dce2f45cc5cd2e
Sina Kuhestani <sinakuhestani@gmail.com>
2025-04-06 13:52:03 +0330

    feat: read from and write to config files including json, yaml, ini and xml.
    Signed-off-by: Sina Kuhestani <sinakuhestani@gmail.com>

---
+++ src/FileUtility.php
@@ -0,0 +1,171 @@
+<?php
+
+/*
+ * This file is part of the PHPallas package.
+ *
+ * (c) Sina Kuhestani <sinakuhestani@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPallas\Utilities;
+
+class FileUtility
+{
+    /**
+     * Reads data from a JSON file.
+     *
+     * @param string $file The path to the JSON file.
+     * @return array|null The decoded data as an associative array, or null on failure.
+     */
+    public static function readFromJson($file)
+    {
+        if (!file_exists($file))
+        {
+            return null;
+        }
+        $jsonContent = file_get_contents($file);
+        if ($jsonContent === false)
+        {
+            return null;
+        }
+        $data = json_decode($jsonContent, true);
+        return $data;
+    }
+
+    /**
+     * Writes data to a JSON file.
+     *
+     * @param array $data The data to be encoded and written to the file.
+     * @param string $file The path to the JSON file.
+     * @return bool True on success, false on failure.
+     */
+    public static function writeToJson($data, $file)
+    {
+        $jsonContent = json_encode($data, JSON_PRETTY_PRINT);
+        
+        if ($jsonContent === false)
+        {
+            return false;
+        }
+
+        if (!is_dir(dirname($file)))
+        {
+            return false; // Return false if the directory does not exist
+        }
+
+        return file_put_contents($file, json_encode($data)) !== false;
+    }
+
+    /**
+     * Reads data from a YAML file.
+     *
+     * @param string $file The path to the YAML file.
+     * @return array|null The parsed data as an associative array, or null on failure.
+     */
+    public static function readFromYaml($file)
+    {
+        if (!file_exists($file))
+        {
+            return null;
+        }
+        $yamlContent = file_get_contents($file);
+        if ($yamlContent === false)
+        {
+            return null;
+        }
+        $data = Polyfill::yaml_parse($yamlContent);
+        return $data;
+    }
+
+    /**
+     * Writes data to a YAML file.
+     *
+     * @param array $data The data to be encoded and written to the file.
+     * @param string $file The path to the YAML file.
+     * @return bool True on success, false on failure.
+     */
+    public static function writeToYaml($data, $file)
+    {
+        $yamlContent = Polyfill::yaml_emit($data);
+        if ($yamlContent === false)
+        {
+            return false;
+        }
+        $result = file_put_contents($file, $yamlContent);
+        return $result !== false;
+    }
+
+    /**
+     * Reads data from an INI file.
+     *
+     * @param string $file The path to the INI file.
+     * @return array|null The parsed data as an associative array, or null on failure.
+     */
+    public static function readFromIni($file)
+    {
+        if (!file_exists($file))
+        {
+            return null;
+        }
+        $data = parse_ini_file($file, true);
+        return $data;
+    }
+
+    /**
+     * Writes data to an INI file.
+     *
+     * @param array $data The data to be encoded and written to the file.
+     * @param string $file The path to the INI file.
+     * @return bool True on success, false on failure.
+     */
+    public static function writeToIni($data, $file)
+    {
+        $iniContent = '';
+        foreach ($data as $section => $values)
+        {
+            $iniContent .= "[$section]\n";
+            foreach ($values as $key => $value)
+            {
+                $iniContent .= "$key = \"$value\"\n";
+            }
+        }
+        $result = file_put_contents($file, $iniContent);
+        return $result !== false;
+    }
+
+    /**
+     * Reads data from an XML file.
+     *
+     * @param string $file The path to the XML file.
+     * @return array|null The parsed data as an associative array, or null on failure.
+     */
+    public static function readFromXml($file)
+    {
+        if (!file_exists($file))
+        {
+            return null;
+        }
+        $xmlContent = file_get_contents($file);
+        if ($xmlContent === false)
+        {
+            return null;
+        }
+        return Polyfill::xmlToArray($xmlContent); // Convert XML to array
+    }
+
+    /**
+     * Writes data to an XML file.
+     *
+     * @param array $data The data to be encoded and written to the file.
+     * @param string $file The path to the XML file.
+     * @return bool True on success, false on failure.
+     */
+    public static function writeToXml($data, $file)
+    {
+        $xmlContent = Polyfill::arrayToXml($data); // Convert array to XML
+        $result = file_put_contents($file, $xmlContent);
+        return $result !== false;
+    }
+}
